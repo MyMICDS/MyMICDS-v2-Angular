6b2bc8ba926838faae16c0f8abd274fdf464f300
Michael Gira <michaelgira23@gmail.com>
2016-07-13 22:04:12 -0500

    Initialize Hello World in Angular 2

---
+++ app/app.ts
@@ -0,0 +1,12 @@
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'mymicds-app',
+	template: `<h1>Hello World!</h1>`
+})
+export class App {
+
+}
+
+bootstrap(App);
