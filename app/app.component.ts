55b229d64768d9b023a5b60cbc74849e8baae37a
Michael Gira <michaelgira23@gmail.com>
2016-07-14 23:09:28 -0500

    Add Navbar component and html

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,18 @@
 import {Component} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 
+import {NavbarComponent} from './components/Navbar/navbar.component';
 import {HomeComponent} from './components/Home/home.component';
 import {LunchComponent} from './components/Lunch/lunch.component';
 
 @Component({
 	selector: 'mymicds-app',
 	template: `
-		<h1>Hello World!</h1>
-		<a [routerLink]="['/home']">Home</a>
-		<a [routerLink]="['/lunch']">Lunch</a>
+		<navbar></navbar>
 		<router-outlet></router-outlet>
 	`,
-	directives: [ROUTER_DIRECTIVES],
-	precompile: [HomeComponent, LunchComponent]
+	directives: [NavbarComponent, ROUTER_DIRECTIVES],
+	precompile: [NavbarComponent, HomeComponent, LunchComponent]
 })
 export class AppComponent {
 
---
+++ app/components/Navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
+
+@Component({
+	selector: 'navbar',
+	templateUrl: 'app/components/navbar/navbar.html',
+	directives: [ROUTER_DIRECTIVES],
+})
+export class NavbarComponent {
+
+}
