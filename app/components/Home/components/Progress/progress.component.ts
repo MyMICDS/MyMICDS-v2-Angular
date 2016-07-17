4cd912c41bd276530623f45c7d1ab680952d6bd7
Michael Gira <michaelgira23@gmail.com>
2016-07-17 02:36:43 -0500

    More work on progress component

--- app/components/Home/components/Progress/progress.component.ts
+++ app/components/Home/components/Progress/progress.component.ts
@@ -11,15 +11,24 @@ import {PortalService} from '../../../../services/portal.service';
 	providers: [PortalService]
 })
 export class ProgressComponent {
+	progressClasses: string[] = ['School'];
+	progressPercentages: number[] = [50];
 
 	constructor(portalService: PortalService) {
+
 		portalService.getSchedule({
 			year: 2016,
 			month: 5,
 			day: 23
 		}).subscribe(
-			(schedule) => {
-				console.log(schedule);
+			(schedule:any) => {
+				let scheduleClasses = schedule.schedule.classes;
+				for(let i = 0; i < scheduleClasses.length; i++) {
+					let scheduleClass = scheduleClasses[i];
+					console.log(scheduleClass);
+					this.progressClasses.push(scheduleClass.name);
+				}
+				console.log('Full classes', this.progressClasses);
 			},
 			(error) => {
 				console.error(error);
@@ -28,14 +37,12 @@ export class ProgressComponent {
 	}
 
 	// Circular Progress Bar
-	public progressOptions = {
+	progressOptions = {
 		legend: {
 			display: false,
 			fullWidth: false
 		},
 		borderColor: 'rgba(0,0,0,0)'
 	};
-	public progressClasses = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-	public progressPercentages = [350, 450, 101];
 
 }
