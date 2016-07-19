6707f1ce908e1ace129ae1bfe1b5bd60ccbf295d
Michael Gira <michaelgira23@gmail.com>
2016-07-19 02:01:40 -0500

    Try to get progress bar working

--- app/components/Home/components/Progress/progress.component.ts
+++ app/components/Home/components/Progress/progress.component.ts
@@ -3,14 +3,15 @@ import {NgIf} from '@angular/common';
 import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
 import {string_to_color} from '../../../../common/string-to-color';
 
-import {DayRotation} from '../../../../pipes/day-rotation';
+import {DayRotation} from '../../../../pipes/day-rotation.pipe';
+import {SchoolPercentage} from '../../../../pipes/school-percentage.pipe';
 
 @Component({
 	selector: 'progress-day',
 	templateUrl: 'app/components/Home/components/Progress/progress.html',
 	styleUrls: ['dist/app/components/Home/components/Progress/progress.css'],
 	directives: [CHART_DIRECTIVES, NgIf],
-	pipes: [DayRotation]
+	pipes: [DayRotation, SchoolPercentage]
 })
 export class ProgressComponent {
 
@@ -39,7 +40,7 @@ export class ProgressComponent {
 	progressPercentages:number[] = [100];
 
 	// Array of colors
-	progressColors:any = [{
+	progressColors:any[] = [{
 		backgroundColor: [],
 		borderColor: ['rgba(0,0,0,0)'],
 		borderWidth: [0]
@@ -50,12 +51,14 @@ export class ProgressComponent {
 	 */
 
 	timer:any;
-	constructor() {
+	ngOnInit() {
+
 		// Start timer
 		console.log('Init progress bar timer');
-		this.timer = setInterval(() => {
+		this.calculatePercentages();
+		this.timer = setTimeout(() => {
 			this.calculatePercentages();
-		}, 100);
+		}, 1000);
 	}
 
 	ngOnDestroy() {
@@ -69,33 +72,94 @@ export class ProgressComponent {
 
 	currentClass:string = null;
 	currentClassPercent:number = null;
+	schoolPercent:number = null;
 
 	calculatePercentages() {
+		// console.log(this.schedule);
 
 		// Fallback if schedule is not set or no school
 		if(!this.schedule || this.schedule.classes.length === 0) {
+			this.progressOptions = {
+				legend: {
+					display: false,
+					fullWidth: false
+				},
+				cutoutPercentage: 95
+			};
+
 			this.currentClass = null;
 			this.currentClassPercent = null;
 
 			this.progressClasses = [];
 			this.progressPercentages = [100];
 			this.progressColors = [{
-				backgroundColor: [],
+				backgroundColor: ['#123'],
 				borderColor: ['rgba(0,0,0,0)'],
 				borderWidth: [0]
 			}];
 			return;
 		}
 
+		// Get length of school
+		let classCount = this.schedule.classes.length;
+		let firstBlock = this.schedule.classes[0];
+		let lastBlock = this.schedule.classes[classCount - 1];
+		let schoolLength = lastBlock.end.getTime() - firstBlock.start.getTime();
+		let schoolPercent = this.getPercent(firstBlock.start, lastBlock.end);
+
+		this.schoolPercent = +schoolPercent.toFixed(2);
+
+		this.progressOptions.circumference = 2 * Math.PI * (schoolPercent / 100);
+
 		// Loop through classes and calculate stuff
-		// Thanks to Amaan for helping me figure out this algorithim back in v1
 		for(let i = 0; i < this.schedule.classes.length; i++) {
 			let block = this.schedule.classes[i];
 
+
+			// Get class percentage
+			// Thanks to Amaan for helping me figure out this algorithim back in v1
 			let classLength = block.end.getTime() - block.start.getTime();
-			// console.log(classLength);
+			let classRatio = classLength / schoolLength;
+			let classPercent = this.getPercent(block.start, block.end);
+			let finalPercentage = classPercent * classRatio;
+
+			// Add values to their respective array
+			this.progressClasses[i] = block.name;
+			this.progressColors[0].backgroundColor[i] = '#' + string_to_color(block.name, 40);
+			this.progressPercentages[i] = +finalPercentage.toFixed(2);
+
+			// If
+
 		}
 
+		// Cut out any classes that could have been removed since last update
+		this.progressClasses.length = classCount;
+		this.progressColors[0].backgroundColor.length = classCount;
+		this.progressPercentages.length = classCount;
+
+		console.log(this.progressClasses, this.progressColors[0].backgroundColor, this.progressPercentages);
+
+	}
+
+	/*
+	 * Gets percentage of class completed between two date objects relative to the current time.
+	 * Will return 0 if class hasn't started yet or 100 of class has already finished.
+	 */
+
+	getPercent(start, end):number {
+
+		var numerator = Date.now() - start.getTime();
+		var denominator = end.getTime() - start.getTime();
+
+		let answer = (numerator / denominator) * 100;
+
+		if(0 <= answer && answer <= 100) {
+			return answer;
+		} else if(answer < 0) {
+			return 0;
+		} else {
+			return 100;
+		}
 	}
 
 }
---
+++ app/pipes/school-percentage.pipe.ts
@@ -0,0 +1,16 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+/*
+ * Either says 'School Day is XX% Complete' or 'School is not in Session!'
+ */
+
+@Pipe({name: 'schoolPercentage'})
+export class SchoolPercentage {
+	transform(value:number): string {
+		if(0 < value && value < 100) {
+			return 'School Day is ' + value + '% Complete!';
+		} else {
+			return 'School is not in Session!';
+		}
+	}
+}
