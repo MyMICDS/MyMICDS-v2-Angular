c455256214f089ba47cc0e6f8782af1d0db82acf
AsianPsychoBoy <ziyang_99@outlook.com>
2016-07-20 14:48:54 +0800

    added title service

--- app/components/Navbar/navbar.component.ts
+++ app/components/Navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 
 import {LocalStorageService} from '../../services/localStorage.service'
+import {Title} from '@angular/platform-browser'
 
 @Component({
 	selector: 'navbar',
@@ -10,7 +11,7 @@ import {LocalStorageService} from '../../services/localStorage.service'
 	directives: [ROUTER_DIRECTIVES],
 })
 export class NavbarComponent {
-	constructor (private localStorage: LocalStorageService) {
+	constructor (private localStorage: LocalStorageService, private titleService: Title) {
 		this.userName = this.localStorage.getItem('user');
 		this.localStorage.setItem$.subscribe(item => {
 			if (item.index === 'user') {this.userName = item.value;}
@@ -18,6 +19,14 @@ export class NavbarComponent {
 		this.localStorage.removeItem$.subscribe(item => {
 			this.userName = undefined;
 		})
+	}
+
+	changeTitle(route:string) {
+		this.titleService.setTitle('MyMCIDS-'+route);
+	}
+
+	ngOnInit() {
+		this.titleService.setTitle('MyMCIDS-Home');
 	}
 
 	public userName: string
--- app/main.ts
+++ app/main.ts
@@ -2,9 +2,11 @@ import {bootstrap} from '@angular/platform-browser-dynamic';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import {AppComponent} from './app.component';
 import {appRouterProviders} from './app.routes';
+import { Title } from '@angular/platform-browser';
+
 
 bootstrap(AppComponent, [
-	appRouterProviders, disableDeprecatedForms(), provideForms(),
+	appRouterProviders, disableDeprecatedForms(), provideForms(), Title
 ]).then(
 	() => window.console.info( 'Angular finished bootstrapping your application!' ),
 	(error) => {
