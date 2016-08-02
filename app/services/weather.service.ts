d59dd70f3a584a06f08a26d1bbca7e312b444baf
Ccoolboy <michaelgira23@gmail.com>
2016-08-02 17:05:18 -0400

    Weather service now uses /weather/get

--- app/services/weather.service.ts
+++ app/services/weather.service.ts
@@ -1,8 +1,8 @@
 import * as config from '../common/config';
 
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
-import {handleError} from '../common/http-helpers';
+import {Http, RequestOptions} from '@angular/http';
+import {xhrHeaders, handleError} from '../common/http-helpers';
 import '../common/rxjs-operators';
 
 @Injectable()
@@ -11,8 +11,21 @@ export class WeatherService {
 	constructor(private http: Http) {}
 
 	getWeather() {
-		return this.http.get(config.backendURL + '/json/weather.json')
-			.map(res => res.json())
-			.catch(handleError);
+        let body = JSON.stringify({});
+		let headers = xhrHeaders();
+        let options = new RequestOptions({ headers });
+
+        return this.http.post(config.backendURL + '/weather/get', body, options)
+        	.map(res => {
+				let data = res.json();
+
+				// Check if server-side error
+				if(data.error) {
+					throw new Error(data.error);
+				}
+
+				return data.weather;
+			})
+        	.catch(handleError);
 	}
 }
