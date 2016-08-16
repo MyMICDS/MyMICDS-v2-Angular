bbc219dd3487bea45aa452de7e2b4ba22b7af75c
Michael Gira <michaelgira23@gmail.com>
2016-08-15 23:44:07 -0500

    Revert "Responsive navbar?"
    This reverts commit e31c18072f90cf8f2cb3613ce91dc70485dfb55a.

--- app/components/Navbar/navbar.component.ts
+++ app/components/Navbar/navbar.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {ROUTER_DIRECTIVES, Router, NavigationStart} from '@angular/router';
 import {capitalize} from '../../common/utils';
-import {CollapseDirective} from 'ng2-bootstrap/ng2-bootstrap';
 import {FaComponent} from 'angular2-fontawesome/components';
 
 import {UserService} from '../../services/user.service';
@@ -11,12 +10,10 @@ import {Title} from '@angular/platform-browser';
 	selector: 'navbar',
 	templateUrl: 'app/components/Navbar/navbar.html',
 	styleUrls: ['dist/app/components/Navbar/navbar.css'],
-	directives: [ROUTER_DIRECTIVES, CollapseDirective, FaComponent],
+	directives: [ROUTER_DIRECTIVES, FaComponent],
 })
 export class NavbarComponent {
 	constructor (private router: Router, private titleService: Title, private userService: UserService) {}
-
-	navbarCollapsed = true;
 
 	ngOnInit() {
 		// Subscribe to router events to change title
