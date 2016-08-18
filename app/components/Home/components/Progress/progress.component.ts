93ea060762e0b6a67d5c8162060b02682ac35d1d
Michael Gira <contact@michaelgira.me>
2016-08-18 12:50:13 -0500

    Fix circular progress bar

--- app/components/Home/components/Progress/progress.component.ts
+++ app/components/Home/components/Progress/progress.component.ts
@@ -2,6 +2,7 @@ import {Component, Input} from '@angular/core';
 import {NgIf} from '@angular/common';
 import {Chart} from 'chart-js'; // This gives an error for some reason, but trust me, it works.
 import {default as prisma} from 'prisma'; // This gives an error for some reason, but trust me, it works.
+import {hexToRgb} from '../../../../common/utils';
 
 import {DayRotationPipe} from '../../../../pipes/day-rotation.pipe';
 import {SchoolPercentagePipe} from '../../../../pipes/school-percentage.pipe';
@@ -89,7 +90,7 @@ export class ProgressComponent {
 		});
 
 		// Start timer
-		// this.calculatePercentages();
+		this.calculatePercentages();
 		this.timer = setInterval(() => {
 			this.calculatePercentages();
 		}, 1000);
@@ -153,8 +154,13 @@ export class ProgressComponent {
 		// Generate colors for each class
 		for(let i = 0; i < formattedSchedule.length; i++) {
 			if(!formattedSchedule[i].color) {
-				let color = prisma(formattedSchedule[i].name);
-				formattedSchedule[i].color = 'rgba(' + color.rgbaArray[0] + ', ' + color.rgbaArray[1] + ', ' + color.rgbaArray[2] + ', 0.8)';
+				let color = [255, 255, 255];
+				if(formattedSchedule[i].class.name) {
+					color = prisma(formattedSchedule[i].class.color).rgbaArray;
+				} else {
+					color = prisma(formattedSchedule[i].class).rgbaArray;
+				}
+				formattedSchedule[i].color = 'rgba(' + color[0] + ', ' + color[1] + ', ' + color[2] + ', 0.8)';
 			}
 		}
 
@@ -174,6 +180,11 @@ export class ProgressComponent {
 		for(let i = 0; i < formattedSchedule.length; i++) {
 			let block = formattedSchedule[i];
 
+			let blockName = block.name;
+			if(block.class) {
+				blockName = block.class.name;
+			}
+
 			// Get class percentage
 			// Thanks to Amaan for helping me figure out this algorithim back in v1
 			let classLength = block.end.getTime() - block.start.getTime();
@@ -186,26 +197,25 @@ export class ProgressComponent {
 			// Add values to their respective array if data is more than 0
 			if(roundedPercent > 0) {
 				newColors.push(block.color);
-				newData.push();
-				newLabels.push(block.name);
+				newData.push(roundedPercent);
+				newLabels.push(blockName);
 			}
 
 			// Check if class is the current class
 			if(0 < classPercent && classPercent < 100) {
-				newCurrentClass = block.name;
+				newCurrentClass = blockName;
 				newCurrentClassPercent = +classPercent.toFixed(2);
 			}
 		}
-
-		// Set current class labels in the middle of the progress bar
-		this.currentClass = newCurrentClass;
-		this.currentClassPercent = newCurrentClassPercent;
 
 		// Add a filler block for when school isn't complete yet
 		newColors.push('rgba(0, 0, 0, 0.1)');
 		newData.push(100 - +schoolPercent.toFixed(2));
 		newLabels.push('School Left');
 
+		// Set current class labels in the middle of the progress bar
+		this.currentClass = newCurrentClass;
+		this.currentClassPercent = newCurrentClassPercent;
 
 		// Assign new arrays to progress bar data
 		this.progressBar.data.datasets[0].backgroundColor = newColors;
@@ -223,7 +233,7 @@ export class ProgressComponent {
 
 	getPercent(start, end):number {
 
-		var numerator = new Date(2016, 4, 23, 11, 30).getTime() - start.getTime();
+		var numerator = new Date().getTime() - start.getTime();
 		var denominator = end.getTime() - start.getTime();
 
 		let answer = (numerator / denominator) * 100;
