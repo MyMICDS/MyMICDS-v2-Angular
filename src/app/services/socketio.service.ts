6e5c39fa7ddfcec035b23916f97cbf5448ae6743
AsianPsychoBoy <ziyang_99@outlook.com>
2016-09-19 03:24:33 +0800

    get rid of socket.io error
    used require instead of import

--- src/app/services/socketio.service.ts
+++ src/app/services/socketio.service.ts
@@ -3,7 +3,7 @@ import { environment } from '../../environments/environment';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
-import * as io from 'socket.io-client';
+var io = require('socket.io-client');
 
 @Injectable()
 export class SocketioService {
