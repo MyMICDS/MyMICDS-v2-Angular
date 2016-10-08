3795ef48bfe3fe13fd32e2e8999701f0262fb20c
AsianPsychoBoy <ziyang_99@outlook.com>
2016-10-07 20:57:16 -0500

    autoscroll on lunch

--- src/app/components/lunch/lunch.component.ts
+++ src/app/components/lunch/lunch.component.ts
@@ -97,6 +97,17 @@ export class LunchComponent implements OnInit {
 			},
 			error => {
 				this.alertService.addAlert('danger', 'Get Lunch Error!', error);
+			},
+			() => {
+				for (let i=0;i<this.lunch.length;i++) {
+					if (this.lunch[i].date.today) {
+						let todayEl;
+						setTimeout(() => {
+							todayEl = document.getElementsByClassName("lunch-day").item(i);
+							todayEl.scrollIntoView({behavior: "smooth"});
+						}, 0);
+					}
+				}
 			}
 		);
 	}
