c946b3a3514f0b6d937af5266cc080c14882b913
Michael Gira <contact@michaelgira.me>
2016-10-13 14:34:08 -0500

    Add loading screen for stats page and refactor logic

--- src/app/components/stats/stats.component.ts
+++ src/app/components/stats/stats.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  Input,
-  trigger,
-  state,
-  style,
-  transition,
-  animate
+	Input,
+	trigger,
+	state,
+	style,
+	transition,
+	animate
 } from '@angular/core';
 
 import { AlertService } from '../../services/alert.service';
@@ -32,33 +32,39 @@ declare let Chart: any;
 			transition('unready => ready', animate('400ms cubic-bezier(0.175, 0.885, 0.32, 1.275)'))
 		]),
 	trigger('switchViews', [
-	  state('visits', style({
-		'margin-top': '-50%'
-	  })),
-	  state('register', style({
-		'margin-top': 'inherit'
-	  })),
-	  transition('register <=> visits', animate('600ms cubic-bezier(0.175, 0.885, 0.32, 1.275)')),
+		state('visits', style({
+			'margin-top': '-50%'
+		})),
+		state('register', style({
+			'margin-top': 'inherit'
+		})),
+		transition('register <=> visits', animate('600ms cubic-bezier(0.175, 0.885, 0.32, 1.275)')),
 	])
 	]
 })
 export class StatsComponent implements OnInit {
 
 	stats: any = null;
+	registeredData: any;
 
+	// Converting graduation years to actual grades
+	gradeRange: number[];
+	gradeNames: string[] = [];
+
+	// Line Chart for registered users over time
 	lineCtx;
 	lineChart;
+	lineDataSets: Object[] = [];
+	lineData: Object[] = [];
+
+	// Pie Chart for percentage of users visited today
 	pieCtx;
 	pieChart;
-	gradeNames: Array<string> = [];
-	lineDataSets: Array<Object> = [];
-	lineData: Array<Object> = [];
-	registeredData: any;
-	gradeRange: Array<number>;
-	pieData: Array<number> = [];
-	pieDataSets: Array<Object> = [];
-	pieBgColors: Array<string> = [];
-	viewingVisits: boolean = false;
+	pieData: number[] = [];
+	pieDataSets: Object[] = [];
+	pieBgColors: string[] = [];
+
+	viewingVisits = false;
 
 	constructor(
 		private alertService: AlertService,
@@ -67,62 +73,85 @@ export class StatsComponent implements OnInit {
 	) { }
 
 	ngOnInit() {
-	let getStats = () => {
+		this.userService.gradeRange().subscribe(
+			range => {
+				this.gradeRange = range;
+				this.getStats();
+			},
+			error => {
+				this.alertService.addAlert('danger', 'Get Grades Error!', error);
+			}
+		);
+	}
+
+	getStats() {
 		this.statsService.getStats().subscribe(
 			data => {
-				console.log(data);
 				this.registeredData = data;
+
+				// Loop through grades to insert into line chart
 				for (let gradYear in data.registered.gradYears) {
+					// Keep track of total users at each point in time
 					let accountSum = 0;
 					this.lineData = [];
 					let dates = Object.keys(data.registered.gradYears[gradYear]);
-					//sort the dates
+
+					// Sort the dates people registered at
 					let mappedDates = dates.map(function(date, i) {
-							return {index: i, value: new Date(date).valueOf()}
-						})
-						mappedDates.sort((a, b) => {
-							return a.value - b.value;
-						})
-						let sortedDates = mappedDates.map(function(el) {
-							return dates[el.index];
-						})
-						for (let i=0;i<sortedDates.length;i++) {
-							let accountNumber = data.registered.gradYears[gradYear][sortedDates[i]];
-							accountSum += accountNumber;
-							let registerCountDate = moment(sortedDates[i]);
-							this.lineData.push({x: registerCountDate, y:accountSum});
-						}
-						let gradeString = this.gradYearToGradeString(parseInt(gradYear));
-						this.lineDataSets.push({
-							label: gradeString,
-							data: this.lineData,
-							fill: false,
-							backgroundColor: prisma(gradeString).hex,
-							borderColor: prisma(gradeString).hex,
-							pointHoverBackgroundColor: "#fff",
-							pointHoverRadius: 5,
-							pointHoverBorderWidth: 2,
-							pointHitRadius: 10,
-						});
+						return {
+							index: i,
+							value: new Date(date).valueOf()
+						};
+					});
+					mappedDates.sort((a, b) => {
+						return a.value - b.value;
+					});
+					let sortedDates = mappedDates.map(function(el) {
+						return dates[el.index];
+					});
+
+					// Loop through sorted dates and add up total accounts
+					for (let i = 0 ; i < sortedDates.length; i++) {
+						let accountNumber = data.registered.gradYears[gradYear][sortedDates[i]];
+						accountSum += accountNumber;
+						let registerCountDate = moment(sortedDates[i]);
+						this.lineData.push({x: registerCountDate, y:accountSum});
 					}
-					// Process data for pie chart
-					for (let gradYear in data.visitedToday.gradYears) {
-						this.pieData.push(data.visitedToday.gradYears[gradYear]);
-						let gradeString = this.gradYearToGradeString(parseInt(gradYear));
-						this.gradeNames.push(gradeString);
-						this.pieBgColors.push(prisma(gradeString).hex);
-					}
-					console.log(this.pieData, this.gradeNames, this.pieBgColors)
-					this.pieDataSets.push({
-						backgroundColor: this.pieBgColors,
-						data: this.pieData,
-						borderWidth: 0
-					})
-				},
-				e => {
-					console.log(e)
-				},
-				() => {
+
+					let gradeString = this.gradYearToGradeString(parseInt(gradYear));
+
+					// Push data to line chart
+					this.lineDataSets.push({
+						label: gradeString,
+						data: this.lineData,
+						fill: false,
+						backgroundColor: prisma(gradeString).hex,
+						borderColor: prisma(gradeString).hex,
+						pointHoverBackgroundColor:'#fff',
+						pointHoverRadius: 5,
+						pointHoverBorderWidth: 2,
+						pointHitRadius: 10,
+					});
+				}
+
+				// Process data for pie chart
+				for (let gradYear in data.visitedToday.gradYears) {
+					this.pieData.push(data.visitedToday.gradYears[gradYear]);
+					let gradeString = this.gradYearToGradeString(parseInt(gradYear));
+					this.gradeNames.push(gradeString);
+					this.pieBgColors.push(prisma(gradeString).hex);
+				}
+
+				// Push data to pie chart
+				this.pieDataSets.push({
+					backgroundColor: this.pieBgColors,
+					data: this.pieData,
+					borderWidth: 0
+				});
+
+				// Initialize Charts
+				setTimeout(() => {
+					// Initialize Line Chart
 					this.lineCtx = document.getElementById('registerCountChart');
 					this.lineChart = new Chart(this.lineCtx, {
 						type: 'line',
@@ -141,7 +170,9 @@ export class StatsComponent implements OnInit {
 								}]
 							}
 						}
-					})
+					});
+
+					// Initialize Pie Chart
 					this.pieCtx = document.getElementById('visitedTodayChart');
 					this.pieChart = new Chart(this.pieCtx, {
 						type: 'pie',
@@ -149,40 +180,23 @@ export class StatsComponent implements OnInit {
 							labels: this.gradeNames,
 							datasets: this.pieDataSets
 						}
-					})
-				}
-			);
-		}
-		this.userService.gradeRange().subscribe(
-			range => {
-				this.gradeRange = range;
+					});
+				}, 1);
 			},
-			e => {
-				this.gradeRange = undefined;
-			},
-			() => {
-				getStats();
-			});
+			error => {
+				this.alertService.addAlert('danger', 'Get Stats Error!', error);
+			}
+		);
 	}
 
 	gradYearToGradeString(gradYear: number): string {
 		let gradeNumber: number;
-		if (this.gradeRange) {
-			for (let i=0;i<this.gradeRange.length;i++) {
-				if (this.gradeRange[i] === gradYear) {
-					gradeNumber = 12 - i;
-					return 'Grade ' + gradeNumber.toString() + ' (' + gradYear + ')';
-				}
-			}
-			return 'Graduated' + ' (' + gradYear + ')';
-		} else {
-			let currentYear = new Date().getFullYear();
-			gradeNumber = 13 - gradYear + currentYear;
-			if (gradeNumber <= 12) {
+		for (let i = 0; i < this.gradeRange.length; i++) {
+			if (this.gradeRange[i] === gradYear) {
+				gradeNumber = 12 - i;
 				return 'Grade ' + gradeNumber.toString() + ' (' + gradYear + ')';
-			} else {
-				return 'Graduated' + ' (' + gradYear + ')';
 			}
 		}
+		return 'Graduated' + ' (' + gradYear + ')';
 	}
 }
