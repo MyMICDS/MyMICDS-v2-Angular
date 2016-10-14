6f214421b904fa0ea07b366ebf8b8c1ba5fd3c2e
AsianPsychoBoy <ziyang_99@outlook.com>
2016-10-14 11:04:10 -0500

    added  astatic file server for mobiel testing, and added empty robotics component

---
+++ src/app/components/robotics/robotics.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'micds-robotics',
+    templateUrl: 'robotics.component.html',
+    styleUrls: ['robotics.component.scss']
+})
+export class RoboticsComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
