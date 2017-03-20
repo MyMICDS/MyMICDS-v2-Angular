8404d718543382323cd988d0b6970085c8c50ee0
Michael Gira <michaelgira23@gmail.com>
2017-03-19 23:57:57 -0400

    Convert spaces to tabs, reorder suggestion options so "other" is last

--- src/app/components/suggestions/suggestions.component.spec.ts
+++ src/app/components/suggestions/suggestions.component.spec.ts
@@ -4,8 +4,8 @@ import { TestBed, async } from '@angular/core/testing';
 import { SuggestionsComponent } from './suggestions.component';
 
 describe('Component: Suggestions', () => {
-  it('should create an instance', () => {
-    let component = new SuggestionsComponent();
-    expect(component).toBeTruthy();
-  });
+	it('should create an instance', () => {
+		let component = new SuggestionsComponent();
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/services/suggestions.service.spec.ts
+++ src/app/services/suggestions.service.spec.ts
@@ -4,13 +4,13 @@ import { TestBed, async, inject } from '@angular/core/testing';
 import { SuggestionsService } from './suggestions.service';
 
 describe('Service: Suggestions', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [SuggestionsService]
-    });
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [SuggestionsService]
+		});
+	});
 
-  it('should ...', inject([SuggestionsService], (service: SuggestionsService) => {
-    expect(service).toBeTruthy();
-  }));
+	it('should ...', inject([SuggestionsService], (service: SuggestionsService) => {
+		expect(service).toBeTruthy();
+	}));
 });
