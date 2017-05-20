b39c23848e2053d24e0b2c30127b1c604b95decf
stlgolfer <stlgolfer@live.com>
2017-05-20 18:50:59 -0500

    Add page

---
+++ src/app/components/standards/standards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mymicds-standards',
+  templateUrl: './standards.component.html',
+  styleUrls: ['./standards.component.scss']
+})
+export class StandardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
