c54778d42ad2d124043ea0a6471b8a040f28f198
Michael Gira <contact@michaelgira.me>
2017-05-26 01:10:53 -0500

    Update announcement

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
 	// Possibly show announcement (leave announcement as empty string for no announcement!)
 	// tslint:disable-next-line:max-line-length
-	announcement = 'Summer\'s almost here which means working on features for next year! <a href="https://goo.gl/forms/0gF66d1l2vVzJKKr1" target="_blank" class="alert-link">Take this survey for a chance to win $15 and improve MyMICDS!</a> We will email you if you win! As always, feel free to also email <strong>support@mymicds.net</strong> if you have any feedback!';
+	announcement = 'Congratulations to Alexander Dawson for winning the $15! Still want to give feedback? <a href="https://goo.gl/forms/0gF66d1l2vVzJKKr1" target="_blank" class="alert-link">Fill out this form!</a> Don\'t forget to email support@mymicds.net at any time during the summer if you have any sugguestions! <strong>Have a great summer!</strong>';
 	dismissAnnouncement = false;
 	showAnnouncement = true;
 
