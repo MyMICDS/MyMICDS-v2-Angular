671e78c408c68840ea45e958bd91c4ce20bf05ee
Michael Gira <michaelgira23@gmail.com>
2017-07-04 22:48:59 -0500

    Correctly size grid container to fit grid
    This is so we can properly add a bottom margin

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, ViewChild, ViewChildren, ElementRef, QueryList } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IGridsterOptions } from 'angular2gridster';
+import { GridsterItemComponent, IGridsterOptions } from 'angular2gridster';
 
 import { AlertService } from '../../services/alert.service';
 import { ModulesService, Module } from '../../services/modules.service';
@@ -11,7 +11,7 @@ import { ScheduleService } from '../../services/schedule.service';
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
 
 	// Possibly show announcement (leave announcement as empty string for no announcement!)
 	// tslint:disable-next-line:max-line-length
@@ -25,7 +25,12 @@ export class HomeComponent implements OnInit, OnDestroy {
 	moduleLayoutSubscription: any;
 	// Array of module objects from the back-end
 	moduleLayout: Module[];
-	// Grider options
+
+	// Gridster
+	@ViewChild('gridContainer') gridContainer: ElementRef;
+	@ViewChildren('gridItem') gridsterItems: QueryList<GridsterItemComponent>;
+	gridsterItemsSubscription: any;
+	// Gridster options
 	gridsterOptions: IGridsterOptions = {
 		lanes: 4,
 		direction: 'vertical',
@@ -59,9 +64,19 @@ export class HomeComponent implements OnInit, OnDestroy {
 
 	}
 
+	ngAfterViewInit() {
+		// Listen for changes
+		this.gridsterItemsSubscription = this.gridsterItems.changes.subscribe(
+			() => {
+				this.calculateGridHeight();
+			}
+		);
+	}
+
 	ngOnDestroy() {
 		this.moduleLayoutSubscription.unsubscribe();
 		this.routeDataSubscription.unsubscribe();
+		this.gridsterItemsSubscription.unsubscribe();
 	}
 
 	dismissAlert() {
@@ -73,6 +88,16 @@ export class HomeComponent implements OnInit, OnDestroy {
 		setTimeout(() => {
 			this.showAnnouncement = false;
 		}, animationTime - 5);
+	}
+
+	// Because angular2gridster is weird, we need to manually calculate the height and set it for proper bottom margin
+	calculateGridHeight() {
+		let maxHeight = 0;
+		this.gridsterItems.forEach(item => {
+			const height = parseInt(item.$element.style.top, 10) + parseInt(item.$element.style.height, 10);
+			maxHeight = Math.max(maxHeight, height);
+		});
+		this.gridContainer.nativeElement.style.height = `${maxHeight}px`;
 	}
 
 }
