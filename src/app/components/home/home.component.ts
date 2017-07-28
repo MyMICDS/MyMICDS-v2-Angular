cf86abb5af18eced718815e55c13859d0957321f
Michael Gira <contact@michaelgira.me>
2017-07-28 13:24:08 -0500

    Attempt to remove force collapse before saving

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { GridsterComponent, IGridsterOptions } from 'angular2gridster';
+import { GridsterComponent, GridsterItemComponent, IGridsterOptions } from 'angular2gridster';
 
 import { modules } from '../modules/modules-main';
 import { Options } from '../modules/modules-config';
@@ -36,6 +36,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
 	// Gridster component
 	@ViewChild('gridster') gridster: GridsterComponent;
+	@ViewChildren('gridItem') gridItems: QueryList<GridsterItemComponent>;
 	// Gridster options
 	gridsterOptions: IGridsterOptions = {
 		direction: 'vertical',
@@ -126,32 +127,24 @@ export class HomeComponent implements OnInit, OnDestroy {
 		// Recalculate responsive positions because sometimes it doesn't recalculate at certain widths
 		// (like 730px wide area)
 		this.gridster.reload();
+		this.updateModulePositions();
 	}
 
 	// Save current moduleLayout in the back-end
 	saveModules(saveModules = this.moduleLayout) {
 		this.savingModuleLayout = true;
-
-		// Update the current layout so that all modules get collapsed
-		// It's not very optimal, but I haven't found any way to prevent collapsing,
-		// so we might as well do it before saving so what the user actually sees is synced with the database
-		this.updateModuleLayout(JSON.parse(JSON.stringify(saveModules)));
-		// Set a timeout so angular2gridster has time to update
-		setTimeout(() => {
-			this.modulesService.upsert(this.moduleLayout)
-				.subscribe(
-					modules => {
-						this.savingModuleLayout = false;
-						this.updateModuleLayout(modules);
-						this.alertService.addAlert('success', 'Success!', 'Successfully saved module layout!', 3);
-					},
-					error => {
-						this.savingModuleLayout = false;
-						this.alertService.addAlert('danger', 'Save Modules Error!', error);
-					}
-				);
-		// I've found 61 ms works majority of time, but doesn't when there's more modules
-		}, 100);
+		this.modulesService.upsert(this.moduleLayout)
+			.subscribe(
+				modules => {
+					this.savingModuleLayout = false;
+					this.updateModuleLayout(modules);
+					this.alertService.addAlert('success', 'Success!', 'Successfully saved module layout!', 3);
+				},
+				error => {
+					this.savingModuleLayout = false;
+					this.alertService.addAlert('danger', 'Save Modules Error!', error);
+				}
+			);
 	}
 
 	// When the user drops a module label onto the grid
@@ -180,9 +173,13 @@ export class HomeComponent implements OnInit, OnDestroy {
 	}
 
 	// Because for some reason angular2gridster's two-way binding isn't working
-	updateModulePosition(index: number, x: number, y: number) {
-		this.moduleLayout[index].column = x;
-		this.moduleLayout[index].row = y;
+	updateModulePositions() {
+		this.gridItems.forEach((item, index) => {
+			this.moduleLayout[index].column = item.item.x;
+			this.moduleLayout[index].row = item.item.y;
+			this.moduleLayout[index].height = item.item.h;
+			this.moduleLayout[index].width = item.item.w;
+		});
 	}
 
 	deleteModule(index: number) {
