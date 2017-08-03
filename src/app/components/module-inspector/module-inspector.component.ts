3fffd68d82ecd7b88296e1b234eaac82b1e30984
Michael Gira <contact@michaelgira.me>
2017-08-03 18:04:54 -0500

    Save current module type and dimensions in URL for mod insp
    That way the current state persists even if you make changes and Angular
    CLI reloads the page

--- src/app/components/module-inspector/module-inspector.component.ts
+++ src/app/components/module-inspector/module-inspector.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import * as interact from 'interactjs';
 
 import { modules, getDefaultOptions } from '../modules/modules-main';
@@ -14,17 +15,56 @@ export class ModuleInspectorComponent implements OnInit, OnDestroy {
 	moduleNames = Object.keys(modules);
 	modules = modules;
 
-	selectedModuleType = this.moduleNames[0];
+	get selectedModuleType() {
+		return this._selectedModuleType;
+	}
+	set selectedModuleType(value: string) {
+		this._selectedModuleType = value;
+		this.updateURL();
+	}
+	private _selectedModuleType = this.moduleNames[0];
+
 	moduleOptions: Options = getDefaultOptions(this.selectedModuleType);
+
 	// Dimensions of the module (in pixels)
-	moduleWidth = 1000;
-	moduleHeight = 500;
+	get moduleWidth() {
+		return this._moduleWidth;
+	}
+	set moduleWidth(value: number) {
+		this._moduleWidth = value;
+		this.updateURL();
+	}
+	private _moduleWidth = 1000;
+
+	get moduleHeight() {
+		return this._moduleHeight;
+	}
+	set moduleHeight(value: number) {
+		this._moduleHeight = value;
+		this.updateURL();
+	}
+	private _moduleHeight = 500;
+
+	private updateURLTimeout: NodeJS.Timer;
 
 	moduleInteractable: Interact.Interactable;
 
-	constructor() { }
+	constructor(private router: Router, private route: ActivatedRoute) { }
 
 	ngOnInit() {
+
+		// See if there's URL parameters
+		const params = this.route.snapshot.queryParams;
+		if (params.type) {
+			this.selectedModuleType = params.type;
+		}
+		if (params.width) {
+			this.moduleWidth = params.width;
+		}
+		if (params.height) {
+			this.moduleHeight = params.height;
+		}
+
 		this.moduleInteractable = interact('.module-container-container')
 			.resizable({
 				autoScroll: true,
@@ -52,6 +92,20 @@ export class ModuleInspectorComponent implements OnInit, OnDestroy {
 
 	updateOptions(options: Options) {
 		// this.moduleOptions = JSON.parse(JSON.stringify(options));
+	}
+
+	private updateURL() {
+		// Add timeout so inspector doesn't lag out when resizing
+		clearTimeout(this.updateURLTimeout);
+		this.updateURLTimeout = setTimeout(() => {
+			this.router.navigate(['/module-inspector'], {
+				queryParams: {
+					type: this.selectedModuleType,
+					width: this.moduleWidth,
+					height: this.moduleHeight
+				}
+			});
+		}, 500);
 	}
 
 }
