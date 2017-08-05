dfe20e2f0a878a189b46d14f81c1dd71a76a1d1b
Jack Cai <ziyang_99@outlook.com>
2017-08-05 18:11:01 +0800

    fix metrics

--- src/app/components/modules/weather/weather.component.ts
+++ src/app/components/modules/weather/weather.component.ts
@@ -38,7 +38,9 @@ export class WeatherComponent implements OnInit, OnDestroy {
 
 	weather: any = null;
 	subscription: any;
-	@Input() metric = false;
+	@Input() set metric(m) {
+		this.toggleMetric(m);
+	}
 
 	constructor(private alertService: AlertService, private weatherService: WeatherService) { }
 
@@ -59,18 +61,19 @@ export class WeatherComponent implements OnInit, OnDestroy {
 	}
 
 	// Toggle the format of temperatures between Ferinheight and Celcius
-	toggleMetric() {
-		this.metric = !this.metric;
-		if (this.metric) {
-			this.weather.currently.windSpeed = (this.weather.currently.windSpeed * 1.609344).toPrecision(4);
-			this.weather.currently.temperature = ((this.weather.currently.temperature - 32) / 1.8).toPrecision(4);
-			this.weather.daily.data[0].temperatureMax = ((this.weather.daily.data[0].temperatureMax - 32) / 1.8).toPrecision(4);
-			this.weather.daily.data[0].temperatureMin = ((this.weather.daily.data[0].temperatureMin - 32) / 1.8).toPrecision(4);
-		} else {
-			this.weather.currently.windSpeed = (this.weather.currently.windSpeed / 1.609344).toPrecision(4);
-			this.weather.currently.temperature = (this.weather.currently.temperature * 1.8 + 32).toPrecision(4);
-			this.weather.daily.data[0].temperatureMax = (this.weather.daily.data[0].temperatureMax * 1.8 + 32).toPrecision(4);
-			this.weather.daily.data[0].temperatureMin = (this.weather.daily.data[0].temperatureMin * 1.8 + 32).toPrecision(4);
+	toggleMetric(isMetric) {
+		if (this.weather) {
+			if (isMetric) {
+				this.weather.currently.windSpeed = (this.weather.currently.windSpeed * 1.609344).toPrecision(4);
+				this.weather.currently.temperature = ((this.weather.currently.temperature - 32) / 1.8).toPrecision(4);
+				this.weather.daily.data[0].temperatureMax = ((this.weather.daily.data[0].temperatureMax - 32) / 1.8).toPrecision(4);
+				this.weather.daily.data[0].temperatureMin = ((this.weather.daily.data[0].temperatureMin - 32) / 1.8).toPrecision(4);
+			} else {
+				this.weather.currently.windSpeed = (this.weather.currently.windSpeed / 1.609344).toPrecision(4);
+				this.weather.currently.temperature = (this.weather.currently.temperature * 1.8 + 32).toPrecision(4);
+				this.weather.daily.data[0].temperatureMax = (this.weather.daily.data[0].temperatureMax * 1.8 + 32).toPrecision(4);
+				this.weather.daily.data[0].temperatureMin = (this.weather.daily.data[0].temperatureMin * 1.8 + 32).toPrecision(4);
+			}
 		}
 	}
 
