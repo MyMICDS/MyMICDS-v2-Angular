f36f15e8cb10005abaf553591efa2db1d932c373
Michael Gira <contact@michaelgira.me>
2017-08-05 20:58:44 -0500

    Fix module options in case not all options are returned from back-end

--- src/app/components/module-options/module-options.component.ts
+++ src/app/components/module-options/module-options.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { contains } from '../../common/utils';
 import { modules, getDefaultOptions } from '../modules/modules-main';
 import { OptionsConfig, Options } from '../modules/modules-config';
 
@@ -10,20 +11,34 @@ import { OptionsConfig, Options } from '../modules/modules-config';
 export class ModuleOptionsComponent {
 
 	@Input()
+	get type() {
+		return this._type;
+	}
 	set type(name: string) {
-		if (!modules[name]) {
+		if (!modules[name] || name === this.type) {
 			return;
 		}
+		this._type = name;
 		this.optionsConfig = modules[name].options;
 		this.optionKeys = Object.keys(this.optionsConfig);
 
-		// Fall back to default options if none are provided
-		if (!this.options) {
-			this.options = getDefaultOptions(name);
-		}
-	}
+		console.log('set type', name, getDefaultOptions(name));
 
-	@Input() options: Options;
+		// Fall back to default options if none are provided
+		this.options = {};
+	}
+	private _type: string = null;
+
+	@Input()
+	get options() {
+		return this._options;
+	}
+	set options(newOptions: Options) {
+		// Instead of replacing options, just override default options
+		this._options = Object.assign({}, getDefaultOptions(this.type), this.options, newOptions);
+	}
+	private _options: Options = {};
+
 	@Output() optionsChange = new EventEmitter<Options>();
 
 	optionKeys: string[];
@@ -32,7 +47,15 @@ export class ModuleOptionsComponent {
 	constructor() { }
 
 	valueChanged() {
-		this.optionsChange.emit(JSON.parse(JSON.stringify(this.options)));
+		// Loop through all options and only emit ones valid
+		const validOptions = {};
+		for (const optionKey of Object.keys(this.options)) {
+			if (contains(this.optionKeys, optionKey)) {
+				validOptions[optionKey] = this.options[optionKey];
+			}
+		}
+
+		this.optionsChange.emit(validOptions);
 	}
 
 }
