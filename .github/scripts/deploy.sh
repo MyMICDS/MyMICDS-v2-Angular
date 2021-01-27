c0535ee57d116ddd06a869b251a9ce0ea0099b9b
Nick Clifford <nick@nickclifford.me>
2021-01-26 21:41:47 -0600

    build on Actions, upload sourcemaps to Sentry

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -9,24 +9,14 @@ mkdir -p ~/.ssh
 ssh-keyscan -p $PORT $IP >> ~/.ssh/known_hosts
 
 ssh apps@$IP -p $PORT <<EOF
-  cd $DEPLOY_DIR
-  git pull
-  npm ci
-  ng build --prod
-
   # Make backup of current production stuff
   TIMESTAMP=\$(date --rfc-3339=seconds)
   mkdir /home/apps/MyMICDS/production-backups/"\$TIMESTAMP"
   cp -R /var/www/mymicds/mymicds-angular/* /home/apps/MyMICDS/production-backups/"\$TIMESTAMP"
 
-  BUILD_DIR=$DEPLOY_DIR/dist/mymicds-v2-angular
-  # Check for a successful build
-  if [ ! -d "\$BUILD_DIR" ]; then
-    echo "Build failed, exiting"
-    exit 1
-  fi
-
-  # Copy new compiled files into production
+  # Remove old build
   rm -rf /var/www/mymicds/mymicds-angular/*
-  cp -R \$BUILD_DIR/* /var/www/mymicds/mymicds-angular
 EOF
+
+# copy over new build
+scp -P $PORT build/* apps@$IP:/var/www/mymicds/mymicds-angular
