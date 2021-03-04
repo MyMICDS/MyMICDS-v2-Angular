cb0345e8c4648596d18755cc5f6e8724cf582fb7
sam <sbbaumo@yahoo.com>
2021-03-03 21:45:47 -0600

    these icons man...

--- src/app/home/modules/bookmarks/bookmarks.component.ts
+++ src/app/home/modules/bookmarks/bookmarks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
 	selector: 'mymicds-bookmarks',
@@ -21,6 +22,19 @@ export class BookmarksComponent implements OnInit {
 			if (isOutlineIcon) {
 				this.icon = this.icon.substring(0, this.icon.length - 2)
 			}
+			
+			let iconExists = false; // check that the icon currently exists in the Icon pack
+			for (const icon in fas) {
+				if (this.icon == fas[icon].iconName) {
+					iconExists = true;
+					break
+				}
+			}
+
+			if (!iconExists) { // if not, use the default 'bookmark'
+				this.icon = 'bookmark'
+			}
+
 		}
 	}
 
