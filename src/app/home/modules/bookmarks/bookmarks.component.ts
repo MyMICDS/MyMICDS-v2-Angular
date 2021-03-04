c71d4bef2fd7bba4e86d439a30a608aa71b1f5a8
sam <sbbaumo@yahoo.com>
2021-03-03 18:56:56 -0600

    actual fix bookmark icons now

--- src/app/home/modules/bookmarks/bookmarks.component.ts
+++ src/app/home/modules/bookmarks/bookmarks.component.ts
@@ -17,6 +17,8 @@ export class BookmarksComponent implements OnInit {
 		const isOutlineIcon = this.icon.substring(this.icon.length - 2, this.icon.length) == '-o'
 		if (this.icon.includes('fa-') && isOutlineIcon) { // If it's an outline icon from FA4 (fa-envelope-o), remove the 'fa' and 'o'
 			this.icon = this.icon.substring(3, this.icon.length - 2)
+		} else if (this.icon.includes('fa-')) {
+			this.icon = this.icon.substring(3);
 		}
 	}
 
