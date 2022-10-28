14ddcd93ab98ef13b07cfeb5e1c5aa6305f99ff4
Michael Gira <michael@gira.dev>
2022-10-28 14:07:05 -0500

    Add forms module to password reset page

--- src/app/reset-password/reset-password.module.ts
+++ src/app/reset-password/reset-password.module.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
@@ -8,6 +9,12 @@ import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
 	declarations: [ResetPasswordComponent],
-	imports: [CommonModule, ResetPasswordRoutingModule, SharedModule]
+	imports: [
+		CommonModule,
+		ResetPasswordRoutingModule,
+		SharedModule,
+		FormsModule,
+		ReactiveFormsModule
+	]
 })
 export class ResetPasswordModule {}
